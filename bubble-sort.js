ea0668d0209ae500ab01953f0c744628e3bc078c
baris_000 <barisozcetin@gmail.com>
2018-06-02 19:35:57 +0300

    added bubble sort

---
+++ bubble-sort.js
@@ -0,0 +1,12 @@
+function bubbleSort(array) {
+  for (let i = array.length; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
+      }
+    }
+  }
+  return array;
+}
+
+bubbleSort([5, 3, 8, 2, 1, 4]);
